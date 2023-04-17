71ba693c052197b9e0437504ddcf16d10bfca2f0
MarcoBione <marcobione.work@gmail.com>
2023-04-17 16:24:24 +0200

    Axios not working

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -5,6 +5,12 @@
 </template>
 
 <script>
+
+axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0').then((res) => {
+    console.log(res.data);
+});
+
+console.log(cards);
 export default {
     data() {
         return {
