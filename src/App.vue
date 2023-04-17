479e3d8c9c55ffd66b195902c4b020820d7fe66c
MarcoBione <marcobione.work@gmail.com>
2023-04-17 15:54:55 +0200

    Set-up all Vue components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-
+    <HeaderComponent />
+    <MainComponent />
   </div>
 </template>
 
 <script>
-export default {
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
 
+export default {
+  components: {
+    HeaderComponent,
+    MainComponent
+  }
 }
 </script>
 
