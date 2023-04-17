0addaff6e1481d69797761a37f50b4fceccf05b2
MarcoBione <marcobione.work@gmail.com>
2023-04-17 16:57:11 +0200

    Add header logo + title

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
 
+        <header class="d-flex align-item-center p-4">
+            <img class="_mylogo" src="	https://www.yugioh-card.com/eu/wp-content/themes/yugioh/images/logo/Yugioh-EN-DE.svg"
+                alt="YuGiOh-Logo">
+            <h1 class="_mytitle m-0">{{ title }}</h1>
+        </header>
+
     </div>
 </template>
 
@@ -8,7 +14,7 @@
 export default {
     data() {
         return {
-
+            title: 'Yu-Gi-Oh Api Deck',
         }
     }
 }
