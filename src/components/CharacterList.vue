5c3326d40546fd448d785bb8fc2b7f607a932c71
MarcoBione <marcobione.work@gmail.com>
2023-04-18 09:39:18 +0200

    Create @ CharacterCard + imported @ CharacterList

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,23 +1,30 @@
 <template>
     <!-- single card -->
-    <div class="card">
-        <img src="" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+    <section>
+        <div class="row">
+            <CharacterCard />
         </div>
-    </div>
+    </section>
 </template>
 
 <script>
+import { store } from '../store';
+import axios from 'axios';
+import CharacterCard from './CharacterCard.vue';
+
 export default {
+
+    name: 'CharacterList',
+    components: {
+        CharacterCard,
+    },
+
     data() {
         return {
 
         }
-    }
+    },
+
 }
 </script>
 
